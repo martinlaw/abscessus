92a634636a023c5a29b7b8970a7585c3b81fcf19
martinlaw <martin.law@mrc-bsu.cam.ac.uk>
2022-11-11 16:58:06 +0000

    Further analysis

--- R/analysis.R
+++ R/analysis.R
@@ -1,43 +1,64 @@
+#### Load packages and data ####
 librarian::shelf(readxl, survival, survminer)
-getwd()
 dat <- readxl::read_xlsx("data/NTM final dataset NR.xlsx")
-dat
+
+#### Data manipulation/cleaning ####
+dat$infected <- ifelse(test=dat$abscessuss_yes_1, yes="Infected", no="Not infected")
+dat$alive <- ifelse(test=dat$alive_yes_1, yes="Alive", no="Not alive")
+dat$lung.dysfun <- ifelse(test=dat$CLAD_yes_1, yes="Yes", no="No")
+dat$airway.injury <- ifelse(test=dat$airway_injury_yes_1, yes="Injury", no="No Injury")
+
+
+names(dat)[names(dat)=="survival_post_tx"] <- "time"
+dat$status <- -1*(dat$alive_yes_1-1)
+
 
 #### Begin with 2x2 table, treating survival as binary ####
 # ORs or RRs? See
 # https://stats.stackexchange.com/questions/326259/odds-ratios-are-inappropriate-for-a-cross-sectional-or-cohort-study
 # https://stats.stackexchange.com/questions/466450/can-someone-explain-as-simply-as-possible-why-you-use-or-in-retrospective-studie
 
-dat$infected <- ifelse(test=dat$abscessuss_yes_1, yes="Infected", no="Not infected")
-dat$alive <- ifelse(test=dat$alive_yes_1, yes="Alive", no="Not alive")
-
 con <- table(dat$infected, dat$alive)
 addmargins(con)
-
-# Risk of death:
-# infected:
-risk.if.infected <- 2/9  # 0.22
-risk.if.not.infected <- 16/59 # 0.27
-# Risk of death is greater in uninfected patients.
-# Relative risk:
-risk.if.infected/risk.if.not.infected # 0.82.
-
 fisher.test(con) # No difference between the groups
 
+table(dat$alive, dat$infected, dat$lung.dysfun)
 
 #### Survival analysis: ####
+# Simple explanation: https://www.graphpad.com/guides/survival-analysis
 # https://www.emilyzabor.com/tutorials/survival_analysis_in_r_tutorial.html
 # https://rpkgs.datanovia.com/survminer/
 
-head(dat)
-names(dat)[names(dat)=="survival_post_tx"] <- "time"
-dat$status <- -1*(dat$alive_yes_1-1)
 
+# Log-rank test:
 fit1 <- survdiff(Surv(time, status) ~ infected, data = dat)
-str(fit1)
-fit1$p
+fit1$pvalue
 
+# Plot:
 fit <- survfit(Surv(time, status) ~ infected, data = dat)
 fit
 ggsurvplot(fit, data=dat)
+
+# Cox PH:
+cox.no.covar <- coxph(Surv(time, status) ~ infected, data = dat)
+
+
+
+#### Lung dysfunction ####
+con.lung <- table(dat$infected, dat$lung.dysfun)
+addmargins(con.lung)
+fisher.test(con.lung) # No difference between the groups
+
+# Cox PH:
+cox.covar <- coxph(Surv(time, status) ~ infected + lung.dysfun, data = dat)
+
+
+
+
+#### Airway injury ####
+con.airway <- table(dat$infected, dat$airway.injury)
+addmargins(con.airway)
+fisher.test(con.airway) # No difference between the groups
+
+
 
