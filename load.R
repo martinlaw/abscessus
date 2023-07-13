03b102b23034151aa605192f2f99b79cd857f911
martinlaw <martin.law@mrc-bsu.cam.ac.uk>
2023-07-13 14:37:54 +0100

    Added new column (from Nicola Ronan) to data

---
+++ load.R
@@ -0,0 +1,11 @@
+#### First time only: Load data, fully anonymise, then save in data folder ####
+# librarian::shelf(readxl)
+# dat <- readxl::read_xlsx("../NTM final dataset NR.xlsx")
+# dat.time.to.infect <- readxl::read_xlsx("../NTM group July 23.xlsx", range="B1:C9")
+# colnames(dat.time.to.infect) <- c("hospno", "infect.day")
+# dat <- merge(x=dat, y=dat.time.to.infect, all=T)
+# dat[, c(1, ncol(dat))]
+# dat <- dat[-1]
+# write.csv(dat, file="data/abscessus_data.csv", )
+
+
