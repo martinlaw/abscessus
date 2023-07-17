aa6703fa3ed01cd79232d9960383daee85bda2ff
martinlaw <martin.law@mrc-bsu.cam.ac.uk>
2023-07-17 09:37:55 +0100

    adding analysis.R and load.R (rm'ing them from gitignore)

--- R/load.R
+++ R/load.R
@@ -1,7 +1,7 @@
 #### First time only: Load data, fully anonymise, then save in data folder ####
 # librarian::shelf(readxl)
-# dat <- readxl::read_xlsx("../NTM final dataset NR.xlsx")
-# dat.time.to.infect <- readxl::read_xlsx("../NTM group July 23.xlsx", range="B1:C9")
+# dat <- readxl::read_xlsx("../../NTM final dataset NR.xlsx")
+# dat.time.to.infect <- readxl::read_xlsx("../../NTM group July 23.xlsx", range="B1:C9")
 # colnames(dat.time.to.infect) <- c("hospno", "infect.day")
 # dat <- merge(x=dat, y=dat.time.to.infect, all=T)
 # dat[, c(1, ncol(dat))]
