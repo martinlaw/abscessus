970719a76955bdfe93b29421f3e2f205c4081bd1
martinlaw <martin.law@mrc-bsu.cam.ac.uk>
2023-07-17 16:22:00 +0100

    Added time dependent covar analysis, updated CSV file (bug in code that did the merge).

--- R/analysis.R
+++ R/analysis.R
@@ -1,15 +1,14 @@
 #### Load packages and data ####
-librarian::shelf(survival, survminer,here, survival)
-dat <- read.csv("../abscessus/data/abscessus_data.csv")
+librarian::shelf(survival, survminer, here)
+dat <- read.csv(paste0(here::here(), "/data/abscessus_data.csv"))
 
 
 #### Data manipulation/cleaning ####
 dat$infected <- ifelse(test=dat$abscessuss_yes_1, yes="Infected", no="Not infected")
+dat$infected <- factor(dat$infected, levels = c("Not infected", "Infected"))
 dat$alive <- ifelse(test=dat$alive_yes_1, yes="Alive", no="Not alive")
 dat$lung.dysfun <- ifelse(test=dat$CLAD_yes_1, yes="Yes", no="No")
 dat$airway.injury <- ifelse(test=dat$airway_injury_yes_1, yes="Injury", no="No Injury")
-
-
 names(dat)[names(dat)=="survival_post_tx"] <- "time"
 dat$status <- -1*(dat$alive_yes_1-1)
 
@@ -64,38 +63,15 @@ fisher.test(con.airway) # No difference between the groups
 ######### Time dependent covariate analysis ####
 
 # Create columns tstart, tstop for time dependent covariate analysis:
-temp.data <- tmerge(data1=dat, data2=dat, id=X,
-                death=event(time, status),
-                infect.var=tdc(infect.day))
-dat.tdc <- temp.data[, c("X", "tstart", "tstop", "death", "infected", "lung.dysfun")]
-tdc.analysis <- coxph(Surv(tstart, tstop, death) ~ infected, data = dat.tdc)
-#coxph(Surv(tstart, tstop, death) ~ infected + lung.dysfun, data = dat.tdc)
+temp.data <- tmerge(data1=dat,
+                    data2=dat,
+                    id=X,
+                    death=event(time, status),
+                    infect.var=tdc(infect.day))
+dat.tdc <- temp.data[, c("X", "tstart", "tstop", "death", "infect.var")]
+
+tdc.analysis <- coxph(Surv(tstart, tstop, death) ~ infect.var, data = dat.tdc)
 summary(tdc.analysis)
-summary(cox.no.covar)
-
-
-########## Example: From Moore et al #######
-result.heart <- coxph(Surv(futime, fustat) ~ transplant + age + surgery, data=jasa)
-summary(result.heart)
-
-tdata <- jasa[, -c(1:4, 11:14)]
-tdata$futime <- pmax(.5, tdata$futime)
-indx <- {{tdata$wait.time == tdata$futime} & !is.na(tdata$wait.time)}
-tdata$wait.time[indx] <- tdata$wait.time[indx] - .5
-tdata$id <- 1:nrow(tdata)
-sdata <- tmerge(tdata, tdata, id=id,
-                   death = event(futime, fustat),
-                   trans = tdc(wait.time))
-jasa.counting <- sdata[,c(7:11, 2:3)]
-
-
-cbind(sdata$fustat, sdata$death)
-
-
-summary(coxph(Surv(tstart, tstop, death) ~ trans + surgery + age, data=jasa.counting))
-head(jasa.counting)
-
-
 
 
 ####### Landmark analysis (for interest) #######
@@ -107,5 +83,3 @@ dat$infected.114 <- dat$infected=="Infected" & dat$infect.day < 114
 landmark <- coxph(Surv(time, status) ~ infected.114, data = dat, subset=index.114d)
 summary(landmark)
 
-dat$infect.day[66]
-dat[66,]
